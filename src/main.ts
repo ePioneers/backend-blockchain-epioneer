728a410c01bc0b7f249f4e140f3a7027406d6d77
Stiven Loaiza <st@Stivens-MacBook-Pro.local>
2021-10-26 22:37:23 -0500

    Swagger install and config OK

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle('API Blockchain epioneer')
+  .setDescription('API to connect the epioneer frontend wallet or other systems with the Algorand blockchain.')
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-doc', app, document);
   await app.listen(3000);
 }
 bootstrap();
